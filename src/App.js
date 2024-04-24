4c888567e6258cb6c21e803eacaadefb5f263226
Elberreth <unix555@gmail.com>
2024-04-24 16:18:29 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import BookingList from './components/BookingList';
+import CancelBooking from './components/CancelBooking';
+import About from './components/About';
+import Home from './components/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Route path="/" exact component={Home} />
+        <Route path="/booking-list" component={BookingList} />
+        <Route path="/cancel-booking" component={CancelBooking} />
+        <Route path="/about" component={About} />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
+
+
+
+
